93a83ac19762f448e2677c9d63726362661abb70
uxiarc <uxiacastro@gmail.com>
2020-07-07 16:25:52 +0200

    [Muut-41] Updating problematic files

--- frontend/src/main/resources/webapp/src/app/shared/app.menu.config.ts
+++ frontend/src/main/resources/webapp/src/app/shared/app.menu.config.ts
@@ -2,7 +2,7 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
-  {id: 'bands', name: 'BANDS', icon: 'home', route: '/main/bands' },
-  {id: 'events', name: 'EVENTS', icon: 'home', route: '/main/events' },
-  { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
+  {id: 'bands', name: 'BANDS', icon: 'user', route: '/main/bands' },
+  {id: 'events', name: 'EVENTS', icon: 'star', route: '/main/events' },
+
 ];
